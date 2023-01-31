9ba87949f59fcbe9b7d6568cd5174ad4f0570b04
gigiftttgi <76035585+gigiftttgi@users.noreply.github.com>
2023-01-31 20:47:07 +0700

    head

--- App/home.py
+++ App/home.py
@@ -1,5 +1,15 @@
-from flask import Flask,render_template
+# from flask import Flask,render_template
+
+import os
+from flask import Flask,request, render_template
+from werkzeug.utils import secure_filename
+# from werkzeug.datastructures import  FileStorage
+
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'csv'}
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 	
 @app.route('/')
 def Home():
@@ -8,6 +18,21 @@ def Home():
 @app.route('/processing')
 def Processing():
    return render_template("process.html")
+
+def allowed_file(filename):
+   return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def uploader():
+    if request.method == 'POST':
+      f = request.files['file']
+      #   file = secure_filename(f.filename)
+      #   f.save(os.path.join (app.config['UPLOAD_FOLDER'],file))
+      if f and allowed_file(f.filename) :
+         filename = secure_filename(f.filename)
+         f.save(os.path.join (app.config['UPLOAD_FOLDER'],filename))
+         return 'upload file success'
 
 if __name__ == '__main__':
  app.debug = True
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ python 3.0 up
 
 How to run
 1. cd app
-2. python3 -m venv venv
+<!-- 2. python3 -m venv venv -->
 3. venv\Scripts\Activate.ps1
 4. pip install Flask
 5. py home.py
