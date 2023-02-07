924b0c25fc49ab0b800746b067e33c98c5cd7dec
minmeeen <sirapat.mp@gmail.com>
2023-02-07 12:12:43 +0700

    add process

--- App/process.py
+++ App/process.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import pandas as pd
  
 fileitem = form['filename']
  
@@ -7,6 +8,25 @@ fileitem = form['filename']
 if fileitem.filename:
     # strip the leading path from the file name
     fn = os.path.basename(fileitem.filename)
+
+    df  = pd.read_csv("\"" + fn +"\"")   #read data
+    ambi = []
+    sus = []
+
+    
+    for i in range(0,len(df)):
+        dfS = df.sort_values(by=i, axis=1, ascending=False)    #sort by row
+        if 1 <= abs((dfS.iloc[i][0] - dfS.iloc[i][1]) - (dfS.iloc[i][1] - dfS.iloc[i][2])) :    #find ambigious
+            ambi.append(df.iloc[i])
+        elif (dfS.iloc[i][0] - dfS.iloc[i][1]) + (dfS.iloc[i][1] - dfS.iloc[i][2]) >=3:     #still improving
+            sus.append(df.iloc[i])      #find sus
+
+
+    
+    #write csv
+    ambi.to_csv('ambigious.csv', index=False, header=False)
+    sus.to_csv('suspect.csv', index=False, header=False)
+
      
    # open read and write the file into the server
     open(fn, 'wb').write(fileitem.file.read())
